7dc483269648e7869df713fa5f946803358cd595
Alex Schofield <git@ajschof.me>
2024-08-16 16:23:56 +0100

    infra(tf): add version constraints for null and archive

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~>5.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~>3.2.2"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~>2.5.0"
+    }
   }
   backend "s3" {
     bucket = "bentley-project-secrets"
