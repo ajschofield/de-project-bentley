1bbc12702a8fa6d5139440c9d04e5bfabd96581d
Alex Schofield <git@ajschof.me>
2024-08-19 21:32:32 +0100

    infra(tf): add versioning to lambda_code_bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -46,3 +46,10 @@ resource "aws_s3_bucket" "lambda_code_bucket" {
     Name = "Lambda Bucket"
   }
 }
+
+resource "aws_s3_bucket_versioning" "lambda_bucket_versioning" {
+  bucket = aws_s3_bucket.lambda_code_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
