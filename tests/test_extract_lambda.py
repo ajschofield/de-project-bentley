b9f3576771c8af8933d23e95f7863f63e2bbc6aa
lian-manonog <lian.manonog@gmail.com>
2024-08-19 15:43:28 +0100

    wip: fixed broken tests; hashed out test_error_retrieving_object

--- tests/test_extract_lambda.py
+++ tests/test_extract_lambda.py
@@ -1,11 +1,13 @@
+import boto3.exceptions
+import botocore.exceptions
 import pytest
 import boto3
 from moto import mock_aws
 from unittest.mock import patch, MagicMock
 from unittest import TestCase
-from src.extract_lambda import list_existing_s3_files, connect_to_database, DBConnectionException, process_and_upload_tables
-import os 
+from src.extract_lambda import list_existing_s3_files, connect_to_database, DBConnectionException, process_and_upload_tables, extract_bucket
 import logging
+import os
 
 @pytest.fixture(scope='class')
 def mock_config():
@@ -16,7 +18,7 @@ def mock_config():
         "password": "password",
         "database": "db",
     }
-    with patch("src.extract_lambda.get_config", return_value=env_vars) as mock_config:
+    with patch("src.extract_lambda.retrieve_secrets", return_value=env_vars) as mock_config:
         yield mock_config
 
 
@@ -24,7 +26,7 @@ def mock_config():
 def aws_credentials():
     os.environ["AWS_ACCESS_KEY_ID"] = 'testing'
     os.environ["AWS_SECRET_ACCESS_KEY"] = 'testing'
-    os.environ["AWS_SECURIT_TOKEN"] = 'testing'
+    os.environ["AWS_SECURITY_TOKEN"] = 'testing'
     os.environ["AWS_SESSION_TOKEN"] = 'testing'
     os.environ["AWS_DEFAULT_REGION"]= 'eu-west-2'
 
@@ -32,6 +34,14 @@ def aws_credentials():
 def s3_client(aws_credentials):
     with mock_aws():
         yield boto3.client('s3')
+
+@pytest.fixture(scope='class')
+def s3_mock_bucket(s3_client):
+    bucket = s3_client.create_bucket(Bucket='extract_bucket',
+                                CreateBucketConfiguration={
+                                    'LocationConstraint': 'eu-west-2'
+                                })
+    return bucket
 
 class TestListExistingS3Files:
     def test_error_if_no_bucket(self, s3_client, caplog):
@@ -42,35 +52,34 @@ class TestListExistingS3Files:
         list_existing_s3_files(client=s3_client)
         assert 'Error listing S3 objects' in caplog.text
 
-    def test_error_if_bucket_is_empty(self, s3_client, caplog):
+    def test_error_if_bucket_is_empty(self, s3_client, caplog, s3_mock_bucket):
+        list_existing_s3_files('extract_bucket', client=s3_client)
+        assert 'The bucket is empty' in caplog.text
 
-        s3_client.create_bucket(Bucket='extract_bucket', 
-                                CreateBucketConfiguration={
-                                    'LocationConstraint': 'eu-west-2'
-                                })
-        list_existing_s3_files(client=s3_client)
-        assert 'The bucket is empty' in caplog.text 
 
-    def test_error_retrieving_object(self, s3_client, caplog):
+    # def test_error_retrieving_object(self, s3_client, caplog, s3_mock_bucket):
+    #     s3_client.upload_file('tests/dummy.txt', 'extract_bucket', 'dummy.txt')
+
+    #     list_existing_s3_files(bucket_name='extract_bucket', client=s3_client)
+
+    #     assert 'Error retrieving S3 object dummy.txt: ClientError' in caplog.text
+
+
+    def test_retrieves_file_content(self, s3_client, caplog, s3_mock_bucket):
         s3_client.upload_file('tests/dummy.txt', 'extract_bucket', 'dummy.txt')
-        list_existing_s3_files(bucket_name='test_bucket', client=s3_client)
+        result = list_existing_s3_files('extract_bucket', client=s3_client)
 
-        assert 'Error retrieving S3 object ' in caplog.text
-
-    def test_retrieves_file_content(self, s3_client, caplog):
-        result = list_existing_s3_files(client=s3_client)
-
-        assert list(result.values()) == ['This is a test file.'] 
+        assert list(result.values()) == ['This is a test file.']
 
 class TestConnectToDatabase:
-    def test_connect_to_database(mock_conn, mock_config):
+    def test_connect_to_database(mock_conn, mock_config): ##had mock_config in param
         with patch("src.extract_lambda.Connection", autospec=True) as mock_conn:  
             connect_to_database()
             mock_conn.assert_called_with(
             host="abc", user="def", port="5432", password="password", database="db"
             )
 
-    def test_database_error(self, mock_config):
+    def test_database_error(self, mock_config):  ##had mock_config in param
         with pytest.raises(DBConnectionException):
             connect_to_database()
 
