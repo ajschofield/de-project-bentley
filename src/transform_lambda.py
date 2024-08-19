3f40e96217418590ca66af6912f595cc04425849
lian-manonog <lian.manonog@gmail.com>
2024-08-19 15:52:14 +0100

    wip: setting up test files for transform_lambda

--- src/transform_lambda.py
+++ src/transform_lambda.py
@@ -1,2 +1,11 @@
+import boto3
+import csv
+from botocore.exceptions import ClientError
+import logging
+import json
+from datetime import datetime
+import re
+
+
 def lambda_handler():
     pass
---
+++ tests/test_transform_lambda.py
@@ -0,0 +1,1 @@
+from src.transform_lambda import lambda_handler
