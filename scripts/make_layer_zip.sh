024de7d7947f46cf6c0c829dc29eb8298e029576
Alex Schofield <git@ajschof.me>
2024-08-19 15:37:54 +0100

    fix(make_layer_zip): change folder structure of layer.zip

--- scripts/make_layer_zip.sh
+++ scripts/make_layer_zip.sh
@@ -1,7 +1,8 @@
 # Description: Make the zip file for the layer
 
 cd "$(dirname "$0")/.."
-mkdir tmp_python
-pip3 install --upgrade -r requirements.txt -t tmp_python/
-zip -r layer.zip tmp_python
-rm -r tmp_python/
+mkdir -p python/lib/python3.11/site-packages
+pip3 install --upgrade -r requirements.txt -t python/lib/python3.11/site-packages
+rm layer.zip
+zip -r layer.zip python
+rm -r python/
