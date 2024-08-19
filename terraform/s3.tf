e5715bc33d4470ceccb17c6853c3e52d4b1035d3
Alex Schofield <git@ajschof.me>
2024-08-19 21:20:09 +0100

    chore(tf): add tags to s3 buckets

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,14 +1,24 @@
 ### EXTRACT BUCKET SET-UP
 resource "aws_s3_bucket" "extract_bucket" {
   bucket_prefix = "${var.s3_extract_bucket_name}-"
+
+  tags = {
+    Name = "Ingestion Bucket"
+  }
 }
 
 ### TRANSFORM BUCKET SET-UP
 resource "aws_s3_bucket" "transform_bucket" {
   bucket_prefix = "${var.s3_transform_bucket_name}-"
+  tags = {
+    Name = "Transform Bucket"
+  }
 }
 
 ### LAMBDA BUCKET
 resource "aws_s3_bucket" "lambda_code_bucket" {
   bucket_prefix = "${var.s3_code_bucket_name}-"
+  tags = {
+    Name = "Load Bucket"
+  }
 }
