22e7de562e62495e547eeff187d86bf9524ae5ca
Alex Schofield <git@ajschof.me>
2024-08-19 20:22:16 +0100

    feat: create shell script for terraform destroy/apply

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+# Deploy Script
+# Description: Deploy and destroy Terraform
+# WARNING: This will most likely destroy any current infrastructure if protections
+# are not in place. Be careful!
+
+echo "WARNING: This script will destroy any infrastructure for testing."
+echo "It should not be used once a proper deployment has been setup."
+echo "Would you like to continue?"
+
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) cd ../terraform/; terraform destroy -auto-approve; terraform apply -auto-approve; terraform destroy -auto-approve; break;;
+		No ) exit;;
+	esac
+done
