50302044c64e414ffe0435908146bc718bf6bed9
Alex Schofield <git@ajschof.me>
2024-08-19 20:24:35 +0100

    feat(deploy.sh): exit if any command returns non-zero status

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@
 # WARNING: This will most likely destroy any current infrastructure if protections
 # are not in place. Be careful!
 
+# Exit if any command has a non-zero status
+set -e
+
 echo "WARNING: This script will destroy any infrastructure for testing."
 echo "It should not be used once a proper deployment has been setup."
 echo "Would you like to continue?"
