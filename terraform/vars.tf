a9fb82f5c96e0ba98d6d3453ce900f2ca22157ec
Alex Schofield <git@ajschof.me>
2024-08-19 21:48:00 +0100

    infra(tf): remove unused declaration in vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -33,11 +33,6 @@ variable "project_name" {
   default = "tt"
 }
 
-variable "aws_region" {
-  type    = string
-  default = "eu-west-2"
-}
-
 variable "environment" {
   type    = string
   default = "dev"
