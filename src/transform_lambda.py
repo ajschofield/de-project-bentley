29eace351c8e35d104992119a3762ab07be1f95d
lian-manonog <lian.manonog@gmail.com>
2024-08-19 16:49:06 +0100

    wip: added read_csb functionailty to lambda_handler

--- src/transform_lambda.py
+++ src/transform_lambda.py
@@ -1,11 +1,35 @@
-import boto3
-import csv
-from botocore.exceptions import ClientError
-import logging
 import json
-from datetime import datetime
-import re
+import boto3
+import io
+from io import StringIO
+import pandas as pd
 
 
-def lambda_handler():
-    pass
+##add trigger window from extract bucket (on console?)
+##suffix: must .csv --> reads only this file type that is uploaded to extract
+##In-order to use PANDAS module in lambda function, a Lambda Layer needs to be attached to the AWS Lambda Function.
+##need a function that normalises the data
+
+
+s3_client = boto3.client('s3')
+
+def lambda_handler(event, context):
+    try:
+        s3_bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+        s3_file_name = event["Records"][0]["s3"]["object"]["key"]
+        
+        object = s3_client.get_object(Bucket=s3_bucket_name, Key=s3_file_name)
+        body = object['Body']
+        csv_string = body.read().decode('utf-8')
+        dataframe = pd.read_csv(StringIO(csv_string)) ##this is the streaming body
+        
+        print(dataframe.head(3))
+
+    except Exception as err:
+        print(err)
+        
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('')
+    }
