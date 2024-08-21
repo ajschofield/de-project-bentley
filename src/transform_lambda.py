b882bb03882ce91c25880defb1461bfbd09dce43
Ang Bel <anzelikabelotelova@Anzelikas-MacBook-Air.local>
2024-08-21 15:48:41 +0100

    complete version of read from s3 subfolder

--- src/transform_lambda.py
+++ src/transform_lambda.py
@@ -6,40 +6,9 @@ import io
 from io import StringIO
 import pandas as pd
 
-##add trigger window from extract bucket (on console?)
-##suffix: must .csv --> reads only this file type that is uploaded to extract
-##In-order to use PANDAS module in lambda function, a Lambda Layer needs to be attached to the AWS Lambda Function.
-##need a function that normalises the data
-
-#s3_resource = boto3.resource('s3') ##need this for a way of reuploading data after transformation
-
 def lambda_handler(event, context):
-    s3_client = boto3.client('s3')  
-    try:
-        s3_bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
-        s3_file_name = event["Records"][0]["s3"]["object"]["key"]
+    pass
 
-        ## concatanating the file per table - most recent
-        ## iterate through the subfolders
-        ## table name prefix to iterate through the files written to that table
-
-        object = s3_client.get_object(Bucket=s3_bucket_name, Key=s3_file_name)
-        body = object['Body']
-        csv_string = body.read().decode('utf-8')
-        dataframe = pd.read_csv(StringIO(csv_string)) ##this is the streaming body
-        
-        print(dataframe.head(3))
-
-    except Exception as err:
-        print(err)
-        
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('')
-    }
-
-## Started from fresh on Wed 21st Aug:
 
 tables = ['sales_order', 
         'transaction', 
