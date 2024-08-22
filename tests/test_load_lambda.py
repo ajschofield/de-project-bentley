828e8292440d4395fbb00afff4e35ff194f07a95
Ellie <ecsymonds@gmail.com>
2024-08-22 16:56:15 +0100

    wip: add test file for load lambda

---
+++ tests/test_load_lambda.py
@@ -0,0 +1,9 @@
+import boto3
+import pandas as pd
+import pyarrow.parquet as pq
+from io import BytesIO
+from src.load_lambda import convert_parquet_files_to_dataframes
+
+class TestConvertParquetToDFs:
+    def test_convert_parquet_to_dfs_returns_df():
+        
