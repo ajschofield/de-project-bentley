a6765659cbeffeae48111f0797d3b4d0752ae80c
Ellie <ecsymonds@gmail.com>
2024-08-27 12:02:19 +0100

    add test progress

--- tests/test_load_lambda.py
+++ tests/test_load_lambda.py
@@ -18,7 +18,7 @@ from src.load_lambda import (
 def aws_credentials():
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-    os.environ["AWS_SECURIT_TOKEN"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
@@ -88,5 +88,6 @@ class TestConvertParquetToDfs:
     #     assert "dim_staff" in result
 
 
-class TestUploadDfsToDatabase:
-    pass
+@pytest.fixture
+def mock_parquet_file(mocker):
+    return mocker.patch(src.load_lambda.convert_parquet_files_to_dfs())
