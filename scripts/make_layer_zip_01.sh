abcf3df1913507517f8e2401eb9a79033b755287
Alex Schofield <git@ajschof.me>
2024-08-29 12:34:21 +0100

    chore: update requirements file names in bash scripts

--- scripts/make_layer_zip_01.sh
+++ scripts/make_layer_zip_01.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")/.."
 mkdir -p python_01/lib/python3.11/site-packages
-pip3 install --upgrade -r requirements_01.txt -t python_01/lib/python3.11/site-packages
+pip3 install --upgrade -r requirements_lambda_01.txt -t python_01/lib/python3.11/site-packages
 rm layer_01.zip
 zip -r layer_01.zip python_01
 rm -r python_01/
--- scripts/make_layer_zip_02.sh
+++ scripts/make_layer_zip_02.sh
@@ -3,7 +3,7 @@
 
 cd "$(dirname "$0")/.."
 mkdir -p python_02/lib/python3.11/site-packages
-pip3 install --upgrade -r requirements_02.txt -t python_02/lib/python3.11/site-packages
+pip3 install --upgrade -r requirements_lambda_02.txt -t python_02/lib/python3.11/site-packages
 rm layer_02.zip
 zip -r layer_02.zip python_02
 rm -r python_02/
